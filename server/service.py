323b6d5a11b782c6f1cd2b9a44c65779e620a9a4
Lu Xu <thusimon@gmail.com>
2021-09-05 00:40:41 -0700

    test data storage

--- server/service.py
+++ server/service.py
@@ -14,7 +14,13 @@ class Service(object):
       # delete the data of the same notice time in db
       deleted = self.db[collection_name].delete_many({'nt': '{}-{}-01'.format(year, month_padded)})
       print('{} rows deleted'.format(deleted.deleted_count))
-      self.db[collection_name].insert_many(data_from_web).inserted_ids
-      print('updated {}-{} successfully'.format(year, month))
+      inserted = self.db[collection_name].insert_many(data_from_web).inserted_ids
+      print('updated {} rows for {}-{} successfully in real db'.format(len(inserted), year, month))
+
+      # update the test database
+      deleted = self.db['pd_test'].delete_many({})
+      print('{} rows deleted'.format(deleted.deleted_count))
+      inserted = self.db['pd_test'].insert_many(data_from_web).inserted_ids
+      print('updated {} rows for {}-{} successfully in test db'.format(len(inserted), year, month))
     else:
       print('no data for {}-{}'.format(year, month))
