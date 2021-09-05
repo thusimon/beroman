58ad81f318bdd4c8d6c86ffa52cdb46bb210e593
Lu Xu <thusimon@gmail.com>
2021-09-05 12:22:16 -0700

    remove mongo url

--- server/db.py
+++ server/db.py
@@ -2,11 +2,9 @@ from pymongo import MongoClient
 import sys
 import os
 
-MONGO_URL="mongodb+srv://node-garden:node-garden@cluster0-tfzjm.mongodb.net/uscis?retryWrites=true&w=majority"
-
 def connect_db():
   try:
-    client = MongoClient(MONGO_URL)
+    client = MongoClient(os.environ.get('MONGO_URL'))
     db=client.uscsi
     return db
   except:
